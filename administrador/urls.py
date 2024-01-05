9a1de447684bae076fb0cd50d4e2d5b8400b3968
Rafaavinii <rafaelvinisouza324@gmail.com>
2024-01-04 22:51:20 -0300

    gerenciamento de estabelecimentos

--- administrador/urls.py
+++ administrador/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.administrador_dashboard_view, name='administrador_dashboard'),
+    path('dashboard/', views.administrador_dashboard_view, name='administrador_dashboard'),
+    path('gerenciamento/', views.administrador_estabelecimento_view, name='administrador_estabelecimento'),
 ]
--- administrador/views.py
+++ administrador/views.py
@@ -1,5 +1,6 @@
 from random import randint
 from django.shortcuts import render
+from estabelecimento.models import Estabalecimento
 
 from projeto_lais.consumirXML import XMLparser
 
@@ -9,6 +10,29 @@ def administrador_dashboard_view(request):
     quantidades = [randint(10, 100) for estabelecimento in estabelecimentos]
     bar_data = {'label': nomes, 'quantidade': quantidades}
     return render(request, 'dashboard_adiministracao.html', bar_data)
+
+def administrador_estabelecimento_view(request):
+    estabalecimentos = Estabalecimento.objects.all()
+    filtro = request.GET.get('filtro')
+    print(filtro)
+
+    if filtro == 'codigo':
+        estabalecimentos = estabalecimentos.order_by('codigo')
+
+    lista_estabelecimento = []
+    for estabalecimento in estabalecimentos:
+        lista_estabelecimento.append(
+            {
+                'nome': estabalecimento.nome,
+                'codigo': estabalecimento.codigo
+            }
+        )
+    
+    print(lista_estabelecimento)
+    
+    context = {'estabelecimentos': lista_estabelecimento, 'filtro': filtro}
+
+    return render(request, 'estabelecimento_administracao.html', context)
 
 def obter_estabelecimentos():
     xml_url = 'https://selecoes.lais.huol.ufrn.br/media/estabelecimentos_pr.xml'
